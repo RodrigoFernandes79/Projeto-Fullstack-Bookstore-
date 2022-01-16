890e57b91e687ad87e25a14e91dbb0bceb10c772
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2022-01-16 14:42:19 -0300

    Criando Controller e Service de categoria , Exceptions , FindByid e Criando nova Categoria

---
+++ Bookstore/src/main/java/com/rodrigo/bookstore/exceptions/ApiException.java
@@ -0,0 +1,33 @@
+package com.rodrigo.bookstore.exceptions;
+
+public class ApiException {
+	
+	public String campo;
+	public String message;
+	
+	
+	
+	public ApiException(String campo, String message) {
+
+		this.campo = campo;
+		this.message = message;
+	}
+
+
+
+	public String getCampo() {
+		return campo;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+	
+	
+	
+
+}
