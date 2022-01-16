72a09e9269e8432e1ebb3c1bf70756d82247104b
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2022-01-16 18:03:51 -0300

    Criando POST e GetById de Livros

--- Bookstore/src/main/java/com/rodrigo/bookstore/models/Categoria.java
+++ Bookstore/src/main/java/com/rodrigo/bookstore/models/Categoria.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 
 @Entity
@@ -25,7 +27,7 @@ public class Categoria implements Serializable {
 	private String nome;
 	
 	private String descriçao;
-	
+	@JsonIgnore
 	@OneToMany(mappedBy="categoria")
 	private List<Livro> livros = new ArrayList<>();
 
